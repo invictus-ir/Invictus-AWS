e4997f274bc7d3c56a2bcc15d8090cc73e742221
d0om <benjamin.glz@etik.com>
2023-11-14 13:46:08 +0100

    add requirements for rtd

--- docs/conf.py
+++ docs/conf.py
@@ -32,7 +32,7 @@ extensions = [
     "sphinx.ext.viewcode",
     "sphinx.ext.todo",
     "sphinx.ext.napoleon",
-    "sphinx_rtd_theme"
+    "sphinx_rtd_theme",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
